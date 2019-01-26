7a2464ae7d2772a5e98cd0b2d88bfac55b8ce794
Ricardo Markiewicz <ricardo.markiewicz@patagonian.it>
2019-01-26 18:41:48 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: Container(
+        child: Scaffold(
+          appBar: AppBar(
+            title: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Icon(Icons.cloud),
+                SizedBox(width: 8.0),
+                Text("Mi App Hola Mundo"),
+              ],
+            ),
+          ),
+          body: HomePage(),
+        ),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        header(),
+        list()
+      ],
+    );
+  }
+
+  Widget header() {
+    return Stack(
+      children: <Widget>[
+        Container(
+          height: 100.0,
+          width: double.infinity,
+          child: Image.network(
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Estacion_tren_-_fernandez_oro_-_panoramio.jpg/501px-Estacion_tren_-_fernandez_oro_-_panoramio.jpg",
+            fit: BoxFit.cover,
+          ),
+        ),
+        Container(
+          height: 100.0,
+          width: double.infinity,
+          color: Colors.black38,
+        ),
+        Positioned(
+          left: 16.0,
+          top: 16.0,
+          child: Text(
+            "Fernandez Oro",
+            style: TextStyle(
+              fontSize: 30.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        Positioned(
+          right: 8.0,
+          bottom: 8.0,
+          child: Text(
+            "31C",
+            style: TextStyle(
+              fontSize: 30.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget list() {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        children: <Widget>[
+          dayWidget("Lunes", Icons.wb_sunny, "32C"),
+          dayWidget("Martes", Icons.wb_cloudy, "32C"),
+          dayWidget("Miércoles", Icons.wb_iridescent, "32C"),
+          dayWidget("Jueves", Icons.wb_sunny, "32C"),
+          dayWidget("Viernes", Icons.wb_sunny, "32C"),
+          dayWidget("Lunes", Icons.wb_sunny, "32C")
+        ],
+      ),
+    );
+  }
+
+  Widget dayWidget(String day, IconData iconData, String temp) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Text(day),
+            Icon(iconData),
+            Text(temp),
+          ],
+        ),
+      ),
+    );
+  }
+}
